bb701a846a8e1ca9795862f4ee883c2143a64bfb
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2024-12-07 21:28:54 +0100

    Day_1

---
+++ Realisation/Day_1/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>To-Do List</h1>
+    <input v-model="newTask" placeholder="Add a task" />
+    <button @click="addTask">Add</button>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        <span
+          @click="toggleTask(index)"
+          :style="{
+            textDecoration: task.done ? 'line-through' : 'none',
+            cursor: 'pointer',
+          }"
+        >
+          {{ task.text }}
+        </span>
+        <button @click="deleteTask(index)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim()) {
+        this.tasks.push({ text: this.newTask, done: false });
+        this.newTask = "";
+      }
+    },
+    toggleTask(index) {
+      this.tasks[index].done = !this.tasks[index].done;
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+  },
+};
+</script>
